4c96617d8e57082137181900a32f06e438e539fd
NathanCioppa <nathan.cioppa@gmail.com>
2023-08-21 18:13:59 -0400

    Symphony hat can be dyed

--- Armor/SymphonyHatLayer.cs
+++ Armor/SymphonyHatLayer.cs
@@ -7,6 +7,8 @@ using Terraria.ModLoader;
 using ReLogic.Content;
 using JoJosMod.Players;
 using System.Linq;
+using Terraria.Initializers;
+using Terraria.Graphics.Shaders;
 
 namespace JoJosMod.Armor
 {
@@ -28,11 +30,13 @@ namespace JoJosMod.Armor
             if (drawInfo.drawPlayer.DeadOrGhost) return;
 
             hatTexture ??= ModContent.Request<Texture2D>("JoJosMod/Armor/SymphonyHat");
-            
+            int dyeShader = drawInfo.drawPlayer.dye?[0].dye ?? 0;
+
             const int FrameHeight = 56;
             int[] highFrames = new int[] {7,8,9,14,15,16};
-            int playerFrameY = Main.player[Main.myPlayer].legFrame.Y;
-                        
+            int playerFrameY = Main.player[Main.myPlayer].bodyFrame.Y;
+
+            
 
             var position = drawInfo.Center + new Vector2(drawInfo.drawPlayer.direction > 0 ? -18 : -12, highFrames.Contains(playerFrameY/FrameHeight) ? -49 : -47) - Main.screenPosition;
             position = new Vector2((int)position.X, (int)position.Y);
@@ -41,12 +45,13 @@ namespace JoJosMod.Armor
                 hatTexture.Value,
                 position,
                 null,
-                Lighting.GetColor(drawInfo.drawPlayer.position.ToTileCoordinates()),
+                drawInfo.colorArmorHead,
                 0,
                 drawInfo.rotationOrigin,
                 1f,
                 drawInfo.drawPlayer.direction > 0 ? SpriteEffects.None : SpriteEffects.FlipHorizontally);
 
+            drawData.shader = dyeShader;
             drawInfo.DrawDataCache.Add(drawData);
         }
     }
