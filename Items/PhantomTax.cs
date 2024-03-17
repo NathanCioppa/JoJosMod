64330e13a4db551bbca5f0ca468098209ea36c8c
NathanCioppa <nathan.cioppa@gmail.com>
2024-03-17 13:46:09 -0400

    Added skeleton for new item

---
+++ Items/PhantomTax.cs
@@ -0,0 +1,19 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using JoJosMod.Projectiles;
+
+namespace JoJosMod.Items
+{
+    internal class PhantomTax : ModItem
+    {
+        public override void SetDefaults()
+        {
+            Item.useStyle = ItemUseStyleID.Shoot;
+            Item.shoot = ModContent.ProjectileType<PhantomSkull>();
+            Item.shootSpeed = 5;
+            Item.useTime = 3;
+            Item.useAnimation = 3;
+        }
+    }
+}
---
+++ Projectiles/PhantomSkull.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using JoJosMod.Helpers;
+using System;
+using Terraria.DataStructures;
+
+namespace JoJosMod.Projectiles
+{
+    internal class PhantomSkull : ModProjectile
+    {
+        const int MaxAnimationFrames = 5;
+
+        public override void SetStaticDefaults()
+        {
+            Main.projFrames[Projectile.type] = MaxAnimationFrames;
+        }
+
+        public override void SetDefaults()
+        {
+            Projectile.width = 26; Projectile.height = 32;
+            Projectile.frame = 0;
+        }
+
+        public override void AI()
+        {
+            Projectile.frame = Projectile.frame >= MaxAnimationFrames - 1 ? 0: Projectile.frame + 1;
+            Projectile.rotation = Projectile.velocity.ToRotation();
+            if (Projectile.direction < 0) Projectile.rotation += MathHelper.Pi;
+            Projectile.spriteDirection = -Projectile.direction;
+        }
+    }
+}
